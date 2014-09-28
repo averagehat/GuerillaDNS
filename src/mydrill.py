3896383cb69289cd3d19a2457b21868e64e6ecd4
root <root@localhost>
2014-09-28 01:11:40 -0500

    mydrill.py works webhook takes it in, etc.

--- src/mydrill.py
+++ src/mydrill.py
@@ -7,13 +7,24 @@ mandrill_key = os.environ['MANDRILL_API_KEY']
 inbound = mandrill.Mandrill(mandrill_key).inbound
 
 def add_webhooks():
-    
-    for domain in domain_collection.find({'using' : True}):
-        name = domain['domainName']
-        print inbound.add_domain(domain=name)
-        print inbound.add_route(name, '*', webhook_url)
+    for domain in domain_collection.find({'using' : True, 'mandrillActivated' : {'$exists' : True }}):
+        papa_domain, subdomain = domain['domainName'], domain['subdomain']
+        try:
+            inbound.delete_domain(domain=papa_domain)
+        except: print ''
+        print inbound.add_domain(domain=subdomain)
+        print inbound.add_route(subdomain, '*', webhook_url)
 
-        domain_collection.update({'domainName' : name }, { '$set' : { 'mandrillActivated' : True} } )
+#        domain_collection.update({'domainName' : name }, { '$set' : { 'mandrillActivated' : True} } )
+
+def send_all():
+
+    for domain in domain_collection.find({'using' : True, 'mandrillActivated' : True }):
+        target = '@'.join(['test_raw_send', domain['subdomain']])
+        sender= 'form_raw_send@adamevespecial.umb88.com'
+        print 'Sending from ' , sender, 'to . . . ' , target
+        message = 'From: ' + sender + '\nTo: ' + target + '\nSubject: Some Subject\n\nSome content.'
+        print inbound.send_raw(raw_message=message, to=[target], mail_from=sender)
 
 if __name__ == '__main__':
-    add_webhooks()
+    send_all()
